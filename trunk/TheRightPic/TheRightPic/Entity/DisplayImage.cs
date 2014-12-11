3f38644d479c403b0e6d10c399aca609fec3b318
tdquang7 <tdquang7@0028ef29-4461-9f72-f1b2-9aca095f8704>
2014-12-11 04:13:14 +0000

    Đã release lên store bản tiếng Việt

--- trunk/TheRightPic/TheRightPic/Entity/DisplayImage.cs
+++ trunk/TheRightPic/TheRightPic/Entity/DisplayImage.cs
@@ -9,10 +9,17 @@ namespace NowUSeeIt
 {
     class DisplayImage
     {
-        public string BasePath { get; set; }
-        public string FileName { get; set; }
-        public List<Question> Questions { get; set; }
+        public string BasePath { get; set; } // Đường dẫn chứa hình
+        public string FileName { get; set; } // Tên file hình chứa ảnh
+        public List<Question> Questions { get; set; } // Danh sách các câu hỏi
+                
+        public int TrackCount { get; set; } // Theo dõi số lần được hiển thị của hình
 
+        /// <summary>
+        /// Chuyển một node xml thành hình ảnh hiển thị với các thông tin liên quan như các câu hỏi và đáp án tương ứng
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         public static DisplayImage Parse(XElement node)
         {
             string filename = node.Attribute("FileName").Value;
--- trunk/TheRightPic/TheRightPic/Entity/Question.cs
+++ trunk/TheRightPic/TheRightPic/Entity/Question.cs
@@ -9,11 +9,18 @@ namespace NowUSeeIt
 {
     class Question
     {
-        public string Content { get; set; }
-        public string LabelA { get; set; }
-        public string LabelB { get; set; }
-        public string Answer { get; set; }
+        public string Content { get; set; } // Nội dung câu hỏi
+        public string LabelA { get; set; }  // Câu trả lời ở bên trái
+        public string LabelB { get; set; }  // Câu trả lời ở bên phải
+        public string Answer { get; set; }  // Chứa đáp án của câu hỏi
+        
+        public int TrackCount { get; set; } // Theo dõi câu hỏi này đã được hiển thị bao nhiêu lần
 
+        /// <summary>
+        /// Parse một node xml thành một câu hỏi
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         public static Question Parse(XElement node)
         {
             string content = node.Attribute("Content").Value;
